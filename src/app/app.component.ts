69198c54cea1ff73934dda7b05e76abc78e451a5
Vadim <vadim@us.kostya.net>
2017-12-19 19:46:04 +0200

    Initial push

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr-client';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  private _hubConnection: HubConnection;
+  public async: any;
+  message = '';
+  messages: string[] = [];
+
+  constructor() { }
+
+  public sendMessage(): void {
+    const data = `Sent : ${this.message}`;
+    this._hubConnection.invoke('Send', data);
+    this.messages.push(data);
+  }
+
+  ngOnInit(){
+    this._hubConnection = new HubConnection('/chat');
+
+    this._hubConnection.on('Send', (data: any) => {
+      const received = `Received: ${data}`;
+      this.messages.push(received);
+    })
+
+    this._hubConnection.start().then(() => {
+      console.log('Hub started!')
+    }).catch(err => {
+      console.log('Error while establishing connection');
+    });
+  }
+}
